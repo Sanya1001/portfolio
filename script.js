180617b417855f1e416efc685677b6ba141cf0a1
Sanya <85473792+Sanya1001@users.noreply.github.com>
2021-06-16 22:43:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const btn3 = document.getElementById('btn3');
 const btn4 = document.getElementById('btn4');
 const btn5 = document.getElementById('btn5');
 const btn6 = document.getElementById('btn6');
+const btn7 = document.getElementById('btn7');
+const btn8 = document.getElementById('btn8');
 
 const navbtn = document.getElementById('nav-btn');
 const nav = document.getElementById('nav');
@@ -14,6 +16,8 @@ const projects = document.getElementById('projects');
 const current = document.getElementById('current');
 const leadership = document.getElementById('leadership');
 const art = document.getElementById('art');
+const cert = document.getElementById('cert');
+const prof = document.getElementById('prof');
 
 btn1.addEventListener('click', () => {
     skills.classList.toggle('inactive');
@@ -42,4 +46,12 @@ btn5.addEventListener('click', () => {
 
 btn6.addEventListener('click', () => {
     art.classList.toggle('inactive');
+})
+
+btn7.addEventListener('click', () => {
+    cert.classList.toggle('inactive');
+})
+
+btn8.addEventListener('click', () => {
+    prof.classList.toggle('inactive');
 })
